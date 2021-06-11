d30ae7a200cf1e6bf4b4c5c1d5f3f07c78b166b4
sansingh <san.singhsanjay@gmail.com>
2021-06-11 11:25:24 +0530

    updated repo

--- src/train.cpp
+++ src/train.cpp
@@ -4,24 +4,37 @@ san.singhsanjay@gmail.com
 June-2021
 To train on MNIST color images dataset
 Compile:
-$ 
+$ g++ train.cpp -lmxnet 
 */
 
 // libraries
 #include <iostream>
 #include <mxnet-cpp/MxNetCpp.h>
+#include <chrono>
 
 // namespaces
 using namespace std;
 using namespace mxnet::cpp;
 
 // global constants
-const int BATCH_SIZE = 99; // instances in train data: 41580 (99% of the entire data)
+/*
+Number of instances in train data: 41580 (99% of the entire data)
+Number of instances in validation data: 420 (1% of the entire data)
+Total instances in the entire data: 42000
+*/
+const int BATCH_SIZE = 84;
 const int IMAGE_W = 28;
 const int IMAGE_H = 28;
 const int IMAGE_C = 3;
 const int NUM_CLASSES = 10;
 const int EPOCHS = 20;
+const float LEARNING_RATE = 0.01;
+
+// function to round off double values up to 2 decimal digits
+float round_fig(double d) {
+	float r = (int) (d * 100 + 0.5);
+	return (float) r / 100;
+}
 
 // CNN - architecture
 Symbol SimpleCNN() {
@@ -119,20 +132,51 @@ int main() {
 	cout<<"Declared validation data iterator"<<endl;
 	// initialising CNN
 	auto cnn_net = SimpleCNN();
-	auto *exec = cnn_net.SimpleBind(ctx, args_map);
+	cnn_net.InferArgsMap(ctx, &args_map, args_map);
 	cout<<"Initialised CNN"<<endl;
+	// optimization function
+	Optimizer* optimizer = OptimizerRegistry::Find("adam");
+	optimizer->SetParam("lr", LEARNING_RATE);
+	cout<<"Successfully defined optimizer"<<endl;
+	// preparing runtime
+	auto *exec = cnn_net.SimpleBind(ctx, args_map);
+	auto arg_names = cnn_net.ListArguments();
 	// training model
 	for(int epoch=0; epoch<EPOCHS; epoch++) {
 		int batchCount = 0;
 		trainImgIter.Reset();
+		auto start_time = chrono::system_clock::now();
 		while(trainImgIter.Next()) {
 			auto batchData = trainImgIter.GetDataBatch();
 			// copy batch data to args_map for training
 			batchData.data.CopyTo(&args_map["data"]);
 			batchData.label.CopyTo(&args_map["label"]);
+			exec->Forward(true);
+			exec->Backward();
+			for(size_t i=0; i<arg_names.size(); i++) {
+				if(arg_names[i] == "data" || arg_names[i] == "label")
+					continue;
+				optimizer->Update(i, exec->arg_arrays[i], exec->grad_arrays[i]);
+			}
 			batchCount += 1;
-			cout<<"Epoch: "<<epoch + 1<<" Batch: "<<batchCount<<endl;
+			if(batchCount % 5 == 0)
+				cout<<"Epoch: "<<epoch + 1<<" Batch: "<<batchCount<<endl;
 		}
+		auto end_time = chrono::system_clock::now();
+		float time_taken = round_fig(chrono::duration_cast<chrono::milliseconds> (end_time - start_time).count() / 1000.0);
+		// validating model
+		Accuracy accuracy;
+		valImgIter.Reset();
+		while(valImgIter.Next()) {
+			auto batchData = valImgIter.GetDataBatch();
+			batchData.data.CopyTo(&args_map["data"]);
+			batchData.label.CopyTo(&args_map["label"]);
+			exec->Forward(false);
+			accuracy.Update(batchData.label, exec->outputs[0]);
+		}
+		cout<<"Epoch: "<<epoch + 1<<" | Training Time: "<<time_taken<<" secs | Val Acc: "<<accuracy.Get()<<endl;
 	}
+	delete exec;
+	MXNotifyShutdown();
 	return 0;
 }
